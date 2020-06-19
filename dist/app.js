957aba01c2709ff661ae6004e423859035e4d7bd
Vinicius Maciel <macielvinis@gmail.com>
2020-06-19 15:38:59 -0300

    First aplication with NODE.JS  & ANGULAR

---
+++ dist/app.js
@@ -0,0 +1,8 @@
+var message: string = "Help me, Obi-Wan Kenobi. You are my oly hope!"
+console.log(message)
+
+var episode: number = 4
+episode = '4'
+console.log("This is episode" + 4)
+episode =episode + 1
+console.log("Next episode is" + episode)
